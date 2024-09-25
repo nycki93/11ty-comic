b8429e417445671d60fb097f1800e43575f43974
Nicholas Lamicela <nupanick@gmail.com>
2024-09-25 14:29:39 -0700

    add comment to metadata.js

--- site-data/metadata.js
+++ site-data/metadata.js
@@ -1,11 +1,14 @@
+// This is used to populate the site metadata in the rss feed
+// see: site-root/atom.xml.njk
+
 module.exports = {
-    title: 'website title',
-    description: 'website description',
-    url: 'https://example.com/',
-    language: 'en',
-    author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
+  title: 'website title',
+  description: 'website description',
+  url: 'https://example.com/',
+  language: 'en',
+  author: {
+    name: "Your Name Here",
+    email: "youremailaddress@example.com",
+    url: "https://example.com/about-me/"
+  }
 }
